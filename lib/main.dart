ce9845b38e11623c630f44183aa3f22cfd116ab3
Harold <haroldkess77@gmail.com>
2024-01-11 01:55:37 +0100

    bug fix and name change

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,6 @@ import 'package:intl/intl.dart';
 import 'controller/backoffice/db.dart';
 import 'controller/operation/network_controller.dart';
 
-const String apiUrl = String.fromEnvironment('CLOUDFLARE_API_URL',
-    defaultValue: 'https://api.cloudflare.com/client/v4');
-const String accountId = String.fromEnvironment('CLOUDFLARE_ACCOUNT_ID',
-    defaultValue: 'f3cf0f0442de28182c41a584b8e5cd7c');
-const String token = String.fromEnvironment('CLOUDFLARE_TOKEN',
-    defaultValue: 'YhGxUEPSGO_9O_k9xf3F6ImE-9c8PI-qkwKcrcgA');
-const String apiKey =
-    String.fromEnvironment('CLOUDFLARE_API_KEY', defaultValue: '');
-const String accountEmail = String.fromEnvironment('CLOUDFLARE_ACCOUNT_EMAIL',
-    defaultValue: 'haroldkess77@GMAIL.COM');
-const String userServiceKey =
-    String.fromEnvironment('CLOUDFLARE_USER_SERVICE_KEY', defaultValue: '');
-
-String? cloudflareInitMessage;
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   CheckConnect.networkCheck(false);
@@ -45,14 +30,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     final configs = ImagePickerConfigs();
-    // AppBar text color
     configs.appBarTextColor = Colors.black;
     configs.appBarBackgroundColor = Colors.white;
-    // Disable select images from album
-    // configs.albumPickerModeEnabled = false;
-    // Only use front camera for capturing
-    // configs.cameraLensDirection = 0;
-    // Translate function
     configs.cropFeatureEnabled = true;
     configs.stickerFeatureEnabled = false;
     configs.cameraPickerModeEnabled = Platform.isIOS ? false : true;
@@ -60,7 +39,6 @@ class MyApp extends StatelessWidget {
     configs.albumPickerModeEnabled = true;
     configs.filterFeatureEnabled = true;
     configs.translateFunc = (name, value) => Intl.message(value, name: name);
-    // Disable edit function, then add other edit control instead
     configs.adjustFeatureEnabled = true;
     configs.externalImageEditors['external_image_editor_1'] = EditorParams(
         title: 'Edit Image',
@@ -101,9 +79,10 @@ class MyApp extends StatelessWidget {
                     maxWidth: maxWidth,
                     maxHeight: maxHeight,
                     configs: configs))));
+
     return OverlaySupport.global(
       child: GetMaterialApp(
-        title: 'Verification',
+        title: 'Regenified-verifier-tool',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
